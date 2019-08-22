915ac30d1a70f6749a9f757e3901007ae00dc248
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2019-08-22 10:46:11 -0500

    Operadores, Condicionales, Continue y Break

--- index.php
+++ index.php
@@ -1,33 +1,52 @@
 <?php
 $name = "Miguel Angel Muñoz Pozos";
+$limitMonths = 12;
+
 
 $jobs = [
-[
-'title' => 'PHP developer',
-'description' => ' Este es la descripción para php'
-],
-[
-'title' => 'Python Dev'
-],
-[
-'title' =>  'Devops'
-]
-];
+  [
+    'title' => 'PHP developer',
+    'description' => ' Este es la descripción para php',
+    'visible' => true,
+    'months' => 6
+  ],
+  [
+    'title' => 'Python Dev',
+    'visible' => true,
+    'months' => 5
+  ],
+  [
+    'title' =>  'Devops',
+    'visible' => true,
+    'months' => 10
 
-// $var1 = 1;
-// if ($var1 > 2) {
-  //   echo 'Es mayor que dos';
-  // }else{
-    //   echo 'No es mayor que dos';
-    // }
-    
-    ?>
-    
-    
-    <!doctype html>
-    <html lang="en">
-    
-    <head>
+  ],
+  [
+    'title' =>  'Node Dev',
+    'visible' => true,
+    'months' => 6
+  ],
+  [
+    'title' =>  'Fronted dev',
+    'visible' => true,
+    'months' => 5
+    ]
+  ];
+  
+  // $var1 = 1;
+  // if ($var1 > 2) {
+    //   echo 'Es mayor que dos';
+    // }else{
+      //   echo 'No es mayor que dos';
+      // }
+      
+      ?>
+      
+      
+      <!doctype html>
+      <html lang="en">
+      
+      <head>
       <!-- Required meta tags -->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -38,119 +57,131 @@ $jobs = [
       <link rel="stylesheet" href="style.css">
       
       <title>Resume</title>
-    </head>
-    
-    <body>
+      </head>
+      
+      <body>
       <div class="container">
-        <div id="resume-header" class="row">
-          <div class="col-3">
-            <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
-          </div>
-          <div class="col">
-            <h1><?php echo $name; ?></h1>
-            <h2>PHP Developer</h2>
-            <ul>
-              <li>Mail: hector@mail.com</li>
-              <li>Phone: 1234567890</li>
-              <li>LinkedIn: https://linkedin.com</li>
-              <li>Twitter: @hectorbenitez</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <h2 class="border-bottom-gray" >Carrer Summary</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <div>
-              <h3 class="border-bottom-gray" >Work Experience</h3>
-              <ul>
+      <div id="resume-header" class="row">
+      <div class="col-3">
+      <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+      </div>
+      <div class="col">
+      <h1><?php echo $name; ?></h1>
+      <h2>PHP Developer</h2>
+      <ul>
+      <li>Mail: hector@mail.com</li>
+      <li>Phone: 1234567890</li>
+      <li>LinkedIn: https://linkedin.com</li>
+      <li>Twitter: @hectorbenitez</li>
+      </ul>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col">
+      <h2 class="border-bottom-gray" >Carrer Summary</h2>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col">
+      <div>
+      <h3 class="border-bottom-gray" >Work Experience</h3>
+      <ul>
+      
+      <?php 
+      $idx = 0;
+      $totalMonths = 0;
+      
+      for ($idx = 0; $idx < count($jobs); $idx++){
 
-                <?php 
-                $idx = 0;
+        $totalMonths += $jobs[$idx]['months'];
 
-                for ($idx = 0; $idx < count($jobs); $idx++){
-                  echo ' <li class="work-position">';
-                  echo '<h5>'.$jobs[$idx]['title'].'</h5>';
-                  echo '<p>'.$jobs[$idx]['description'].'</p>';
-                  echo '<strong>Achievements:</strong>';
-                  echo '<ul>';
-                  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                  echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-                  echo '</ul>';
-                  echo '</li>';
-                } ?>
-              </ul>
-            </div>
-            <div>
-              <h3 class="border-bottom-gray">Projects</h3>
-              <div class="project">
-                <h5>Project X</h5>
-                <div class="row">
-                  <div class="col-3">
-                    <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
-                  </div>
-                  <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
-                    <strong>Technologies used:</strong>
-                    <span class="badge badge-secondary">PHP</span>
-                    <span class="badge badge-secondary">HTML</span>
-                    <span class="badge badge-secondary">CSS</span>
-                  </div>
-                </div>
-              </div>
-              <div class="project">
-                <h5>Project X</h5>
-                <div class="row">
-                  <div class="col-3">
-                    <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
-                  </div>
-                  <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
-                    <strong>Technologies used:</strong>
-                    <span class="badge badge-secondary">PHP</span>
-                    <span class="badge badge-secondary">HTML</span>
-                    <span class="badge badge-secondary">CSS</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-3">
-            <h3 class="border-bottom-gray" >Skills & Tools</h3>
-            <h4>Backend</h4>
-            <ul>
-              <li>PHP</li>
-            </ul>
-            <h4>Frontend</h4>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-            </ul>
-            <h4>Frameworks</h4>
-            <ul>
-              <li>Laravel</li>
-              <li>Bootstrap</li>
-            </ul>
-            <h3 class="border-bottom-gray" >Languages</h3>
-            <ul>
-              <li>Spanish</li>
-              <li>English</li>
-            </ul>
-          </div>
-        </div>
-        <div id="resume-footer" class="row">
-          <div class="col">
-            Designed by @hectorbenitez
-          </div>
-        </div>
+        if ($totalMonths > $limitMonths) {
+          break;
+        }
+
+        if ($jobs[$idx]['visible'] == false) {
+            continue;
+        }
+        echo ' <li class="work-position">';
+        echo '<h5>'.$jobs[$idx]['title'].'</h5>';
+        echo '<p>'.$jobs[$idx]['description'].'</p>';
+        echo '<p>'.$totalMonths.'</p>';
+        echo '<strong>Achievements:</strong>';
+        echo '<ul>';
+        echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo '</ul>';
+        echo '</li>';
+      } ?>
+      </ul>
+      </div>
+      <div>
+      <h3 class="border-bottom-gray">Projects</h3>
+      <div class="project">
+      <h5>Project X</h5>
+      <div class="row">
+      <div class="col-3">
+      <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+      </div>
+      <div class="col">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+      <strong>Technologies used:</strong>
+      <span class="badge badge-secondary">PHP</span>
+      <span class="badge badge-secondary">HTML</span>
+      <span class="badge badge-secondary">CSS</span>
+      </div>
+      </div>
+      </div>
+      <div class="project">
+      <h5>Project X</h5>
+      <div class="row">
+      <div class="col-3">
+      <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+      </div>
+      <div class="col">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+      <strong>Technologies used:</strong>
+      <span class="badge badge-secondary">PHP</span>
+      <span class="badge badge-secondary">HTML</span>
+      <span class="badge badge-secondary">CSS</span>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      <div class="col-3">
+      <h3 class="border-bottom-gray" >Skills & Tools</h3>
+      <h4>Backend</h4>
+      <ul>
+      <li>PHP</li>
+      </ul>
+      <h4>Frontend</h4>
+      <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      </ul>
+      <h4>Frameworks</h4>
+      <ul>
+      <li>Laravel</li>
+      <li>Bootstrap</li>
+      </ul>
+      <h3 class="border-bottom-gray" >Languages</h3>
+      <ul>
+      <li>Spanish</li>
+      <li>English</li>
+      </ul>
+      </div>
+      </div>
+      <div id="resume-footer" class="row">
+      <div class="col">
+      Designed by @hectorbenitez
+      </div>
+      </div>
       </div>
       
       <!-- Optional JavaScript -->
@@ -161,6 +192,6 @@ $jobs = [
       crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
       crossorigin="anonymous"></script>
-    </body>
-    
-    </html>
+      </body>
+      
+      </html>
