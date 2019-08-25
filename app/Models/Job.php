372bb347d0b28892ae2c11ae88df186926db7e4a
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2019-08-24 22:40:24 -0500

    Usando el ORM Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,24 +1,19 @@
 <?php 
 namespace App\Models;
-require_once 'BaseElement.php';
+use Illuminate\Database\Eloquent\Model;
 
+class Job extends Model{
 
-class Job extends BaseElement{
-
-	public function __construct($title, $description)
-	{
-		$newTitle =  'Job: '. $title;
-		parent::__construct($newTitle, $description);
-	}
-
+	protected $primaryKey = 'jobs';
+	
 	public function getDurationAsString($months)
 	{
 		$years = floor($this->months / 12);
 		$years = ($years > 0) ? $years . ' years' : '';
-
+		
 		$extraMoths = $this->months % 12;
 		$extraMoths = ($extraMoths > 0) ? $extraMoths . ' months' : '';
-
+		
 		return "Job duration $years $extraMoths";
 	}
 }
