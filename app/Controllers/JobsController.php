6e90834360e8ea47bd6e73c446f77b9faa862322
MiguelAngelMunozPozos <mmunozpozos@gmail.com>
2019-10-08 18:21:27 -0500

    Validaciones

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -1,23 +1,36 @@
-<?php 
+<?php
+
 namespace App\Controllers;
+
 use App\Models\Job;
+use Respect\Validation\Validator as v;
 
-class JobsController extends BaseController {
-    public function getAddJobAction($request){
+class JobsController extends BaseController
+{
+    public function getAddJobAction($request)
+    {
 
+        $responseMessage = null;
 
-        if (($request->getMethod() == 'POST') ) {
-            $job = new Job();
+        if (($request->getMethod() == 'POST')) {
+
             $body = $request->getParsedBody();
-            $job->title = $body['title'];
-            $job->description = $body['description'];
-            $job->save();
+            $jobValidator = v::key('title', v::stringType()->notEmpty())
+                ->key('description', v::stringType()->notEmpty());
+            try {
+                $jobValidator->assert($body);
+                $job = new Job();
+                $job->title = $body['title'];
+                $job->description = $body['description'];
+                $job->save();
+                $responseMessage = 'Saved';
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
-        
-        return $this->renderHTML('addJob.twig');
-        
+
+        return $this->renderHTML('addJob.twig', [
+            'responseMessage' => $responseMessage
+        ]);
     }
 }
-
-
-?>
