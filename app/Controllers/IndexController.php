471d682308aa47000bd6091132869cdfdf4ff97f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2019-12-07 22:50:48 -0600

    usando Closures

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -10,11 +10,15 @@ class IndexController extends BaseController
     public function indexAction()
     {
 
+
         $jobs = Job::all();
         $projects = Project::all();
+        $limitMonths = 10;
+        $jobs = array_filter($jobs->toArray(), function ($job) use ($limitMonths) {
+            return $job['months'] >= $limitMonths;
+        });
 
         $name = "Miguel Angel Muñoz Pozos";
-        $limitMonths = 2000;
 
         return $this->renderHTML('index.twig', [
             'name' => $name,
